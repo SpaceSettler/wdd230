7488cab13844778444eeee40ea4e2ce253200496
SpaceSettler <j.devon.richardson@gmail.com>
2024-05-02 20:24:57 -0700

    home page

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+let input = new Date().toLocaleDateString(options);
+document.getElementById('year').textContent = input;
+input = input.slice(-4);
+document.getElementById('copy').textContent = input;
+document.querySelector("#lastModified").textContent = new Date(document.lastModified);
