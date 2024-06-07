92f56d24fe19d30097bc45bddfe685b6b5db3a6a
SpaceSettler <j.devon.richardson@gmail.com>
2024-06-06 19:06:34 -0700

    Fin?

--- chamber/scripts/hamBut.js
+++ chamber/scripts/hamBut.js
@@ -1,6 +1,21 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('#navigation');
+const banButton = document.querySelector('#bannerBut');
+const banner = document.querySelector('#banner');
+const today = new Date();
+let day = today.getDay()
+
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open')
     hamButton.classList.toggle('open')
 });
+banButton.addEventListener('click', () => {
+    banner.classList.toggle('open')
+});
+
+if (day === 3 || day === 2 || day === 1) {
+    banner.classList.toggle('close');
+} else {
+    banner.classList.toggle('open');
+}
+
